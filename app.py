a36e62543a730ea4c31938042d8b66fdbc1b3e5f
Sourabh Agrawal <agrawal.sourabh644@gmail.com>
2020-06-16 15:49:49 +0530

    mobile view added

--- app.py
+++ app.py
@@ -25,29 +25,55 @@ is_mobile_view_selected = False
 
 @app.route('/')
 def home():
-    form = BasicForm()
-    if form.validate_on_submit():
+    # form = BasicForm()
+    # if form.validate_on_submit():
         
-        rows, columns = getDesktopRowsAndCols()
-        pFinder.set_rows(rows=rows)
-        pFinder.set_columns(columns=columns)
-        pFinder.createGraph()
-        return render_template('home_new.html',rows=rows, columns=columns)
+    #     rows, columns = getDesktopRowsAndCols()
+    #     pFinder.set_rows(rows=rows)
+    #     pFinder.set_columns(columns=columns)
+    #     pFinder.createGraph()
+    #     return render_template('home_new.html',rows=rows, columns=columns)
 
     return render_template('index.html')
-        
     
 
-    
+@app.route('/mobile')
+def openMobileView():
+    rows, columns = getMobileRowsAndCols()
+    pFinder.set_rows(rows=rows)
+    pFinder.set_columns(columns=columns)
+    pFinder.createGraph()
+    print("Mobile")
+    print("Rows")
+    print(str(pFinder.get_rows()))
+    print("Columns")
+    print(str(pFinder.get_columns()))
+    return render_template('home_new.html',rows=pFinder.get_rows(), columns=pFinder.get_columns())
+
+@app.route('/desktop')
+def openDesktopView():
+    rows, columns = getDesktopRowsAndCols()
+    pFinder.set_rows(rows=rows)
+    pFinder.set_columns(columns=columns)
+    pFinder.createGraph()
+    print("Desktop")
+    print("Rows")
+    print(str(pFinder.get_rows()))
+    print("Columns")
+    print(str(pFinder.get_columns()))
+    return render_template('home_new.html',rows=pFinder.get_rows(), columns=pFinder.get_columns())
 
 @app.route('/Dijkstra/<starting_node>/<ending_node>')
 def dijkstra(starting_node, ending_node):
     shortest_path, visited, traverse = pFinder.find_dijsktra_path(initial=starting_node, endNode=ending_node)
     # res = {node: '0' for node in path}
     # return traverse
-
-    rows, columns = getDesktopRowsAndCols()
-    return render_template("home-path.html", rows=rows, columns=columns, shortest_path = shortest_path, distance=len(shortest_path), visited=visited, initial=starting_node, endNode=ending_node, traverse=traverse)
+    print("Dijkstra")
+    print("Rows")
+    print(str(pFinder.get_rows()))
+    print("Columns")
+    print(str(pFinder.get_columns()))
+    return render_template("home-path.html", rows=pFinder.get_rows(), columns=pFinder.get_columns(), shortest_path = shortest_path, distance=len(shortest_path), visited=visited, initial=starting_node, endNode=ending_node, traverse=traverse)
 
 
 @app.route('/Astar/<starting_node>/<ending_node>')
@@ -55,7 +81,12 @@ def astar(starting_node, ending_node):
     shortest_path, traverse = pFinder.find_astar_path(initial=starting_node, endNode=ending_node)
     # res = {node: '0' for node in path}
     # return traverse
-    return render_template("home-path.html", shortest_path = shortest_path, distance=len(shortest_path), visited=None, initial=starting_node, endNode=ending_node, traverse=traverse)
+    print("Astar")
+    print("Rows")
+    print(str(pFinder.get_rows()))
+    print("Columns")
+    print(str(pFinder.get_columns()))
+    return render_template("home-path.html", rows=pFinder.get_rows(), columns=pFinder.get_columns(), shortest_path = shortest_path, distance=len(shortest_path), visited=None, initial=starting_node, endNode=ending_node, traverse=traverse)
 
 
 if __name__ == '__main__':
